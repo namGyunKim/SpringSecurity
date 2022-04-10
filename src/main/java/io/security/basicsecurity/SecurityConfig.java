eada7873100991b6d5b0719c78a3e8b077f05296
namGyunKim <skarbs01@gmail.com>
2022-04-10 17:38:59 +0900

    로그아웃 필터 설정

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -9,10 +9,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 
@@ -26,13 +29,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
 //                인가 정책
                 .authorizeRequests()
-                .anyRequest().authenticated();  //어떠한 요청에도 다 허락
+                .anyRequest().authenticated()  //어떠한 요청에도 다 허락
+        ;
+
 //        인증 정책
         http
                 .formLogin()    //폼 로그인 방식으로
 //                .loginPage("/loginPage")        //사용자 정의 로그인 페이지
-                .defaultSuccessUrl("/")         //로그인 성공 후 이동 페이지
-                .failureUrl("/login")           //로그인 실패 후 이동 페이지
+//                .defaultSuccessUrl("/")         //로그인 성공 후 이동 페이지
+//                .failureUrl("/login")           //로그인 실패 후 이동 페이지
                 .usernameParameter("userId")    //파라메터 아이디 설정
                 .passwordParameter("passwd")    //파라메터 비밀번호 설정
 //                .loginProcessingUrl("login_proc")   //로그인 폼 액션 url
@@ -52,6 +57,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     }
                 })
                 .permitAll()
+        ;
+//        로그아웃
+
+        http
+                .logout()
+                .logoutUrl("/logout")
+//                .logoutSuccessUrl("/login")
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession httpSession =request.getSession();
+                        log.info("세션 무효화");
+                        httpSession.invalidate();           //세션 무효화
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        log.info("로그아웃 성공");
+                        response.sendRedirect("/login");
+                    }
+                })
+                .deleteCookies("remember-me")           //remember-me라는 쿠키를 삭제
+
         ;
 
     }
